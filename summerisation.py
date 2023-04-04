5e6dbb4e83983ce2989301a38a24c76580f6cec1
Dinh Tran <37775547+ttchaudinh@users.noreply.github.com>
2023-04-04 13:39:28 +0200

    Update summerisation.py

--- summerisation.py
+++ summerisation.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
 
 # Set up OpenAI API credentials
-openai.api_key = st.secrets["openai"]
+openai.api_key = st.secrets["openai_key"]
 
 # Define the Streamlit app
 def main():
